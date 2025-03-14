93c96f8a4576dd032a43d0530abed976f017b521
Chyoko000 <c2543510910@163.com>
2025-03-14 10:46:50 +0800

    测试

---
+++ Pgmagrpm/src/test/Backpackproblem.java
@@ -0,0 +1,12 @@
+package test;
+
+import org.junit.Test;
+
+public class Backpackproblem {
+    @Test
+    public void test01(){
+
+}
+
+
+}
---
+++ java03/src/com/situ/demo/test.java
@@ -0,0 +1,9 @@
+package com.situ.demo;
+
+public class test {
+    //sout打印快捷键
+    public static void main(String[] args) {
+        System.out.println("原神启动");
+
+    }
+}
